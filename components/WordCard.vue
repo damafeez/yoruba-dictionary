0dcd9d22e4caf7a0aa6175928d1e3e36c42a3744
Afeez Awoyemi <damafeez@gmail.com>
2020-12-20 18:27:19 +0100

    feat(description): bootstrap description page

--- components/WordCard.vue
+++ components/WordCard.vue
@@ -1,6 +1,7 @@
 <template>
-  <div
-    class="border border-off-white border-solid p-5 rounded-sm cursor-pointer group hover:bg-off-white-2 hover:border-off-white-2 transition-colors"
+  <nuxt-link
+    to="/word-description"
+    class="border border-off-white border-solid no-underline p-5 rounded-sm cursor-pointer group hover:bg-off-white-2 hover:border-off-white-2 transition-colors"
   >
     <h4 class="mt-0 group-hover:text-blue transition-colors">Aburo</h4>
     <p class="text-sm m-0 text-black-2">
@@ -9,5 +10,5 @@
       reiciendis quidem, soluta sint rerum fugiat accusantium perferendis alias
       ratione voluptatibus!
     </p>
-  </div>
+  </nuxt-link>
 </template>
